79031041342152666112a4f06fe2f1a183f0ec9e
Simon Schötz <simonsch.tz@gmail.com>
2024-06-20 19:37:48 +0200

    rough cleanup and adjust intial tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, verso-code-challenge');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Verso Code Challenge'
+    );
   });
 });
